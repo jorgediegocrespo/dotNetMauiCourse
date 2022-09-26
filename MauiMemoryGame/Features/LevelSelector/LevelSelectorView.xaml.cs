dc69e9845c60b76e4205daafbdaf35623c19239d
Jorge DevsDNA <jorge@devsdna.com>
2022-09-26 17:36:55 +0200

    Level selector view and viewmodel

--- MauiMemoryGame/Features/LevelSelector/LevelSelectorView.xaml.cs
+++ MauiMemoryGame/Features/LevelSelector/LevelSelectorView.xaml.cs
@@ -7,4 +7,19 @@ public partial class LevelSelectorView
 		ViewModel = viewModel;
 		InitializeComponent();
 	}
+
+    protected override void CreateBindings(CompositeDisposable disposables)
+    {
+        base.CreateBindings(disposables);
+
+        this.OneWayBind(ViewModel, vm => vm.SelectHighCommand, v => v.btHigh.Command).DisposeWith(disposables);
+        this.OneWayBind(ViewModel, vm => vm.SelectMediumCommand, v => v.btMedium.Command).DisposeWith(disposables);
+        this.OneWayBind(ViewModel, vm => vm.SelectLowCommand, v => v.btLow.Command).DisposeWith(disposables);
+        this.OneWayBind(ViewModel, vm => vm.NavigateBackCommand, v => v.btBack.Command).DisposeWith(disposables);
+
+        this.OneWayBind(ViewModel, vm => vm.IsSelectingHigh, v => v.btHigh.IsBusy).DisposeWith(disposables);
+        this.OneWayBind(ViewModel, vm => vm.IsSelectingMedium, v => v.btMedium.IsBusy).DisposeWith(disposables);
+        this.OneWayBind(ViewModel, vm => vm.IsSelectingLow, v => v.btLow.IsBusy).DisposeWith(disposables);
+        this.OneWayBind(ViewModel, vm => vm.IsNavigatingBack, v => v.btBack.IsBusy).DisposeWith(disposables);
+    }
 }
--- MauiMemoryGame/Features/LevelSelector/LevelSelectorViewModel.cs
+++ MauiMemoryGame/Features/LevelSelector/LevelSelectorViewModel.cs
@@ -7,6 +7,52 @@ public class LevelSelectorViewModel : BaseViewModel
     {
 	}
 
-    public static Themes SelectedTheme { get; private set; }
+    public Themes SelectedTheme { get; private set; }
+
+    public ReactiveCommand<Unit, Unit> SelectHighCommand { get; private set; }
+    public extern bool IsSelectingHigh { [ObservableAsProperty] get; }
+
+    public ReactiveCommand<Unit, Unit> SelectMediumCommand { get; private set; }
+    public extern bool IsSelectingMedium { [ObservableAsProperty] get; }
+
+    public ReactiveCommand<Unit, Unit> SelectLowCommand { get; private set; }
+    public extern bool IsSelectingLow { [ObservableAsProperty] get; }
+
+    protected override void HandleActivation(CompositeDisposable disposables)
+    {
+        base.HandleActivation(disposables);
+
+        SelectHighCommand.IsExecuting.ToPropertyEx(this, x => x.IsSelectingHigh).DisposeWith(disposables);
+        SelectMediumCommand.IsExecuting.ToPropertyEx(this, x => x.IsSelectingMedium).DisposeWith(disposables);
+        SelectLowCommand.IsExecuting.ToPropertyEx(this, x => x.IsSelectingLow).DisposeWith(disposables);
+
+        SelectHighCommand.ThrownExceptions.Subscribe(logService.TraceError).DisposeWith(disposables);
+        SelectMediumCommand.ThrownExceptions.Subscribe(logService.TraceError).DisposeWith(disposables);
+        SelectLowCommand.ThrownExceptions.Subscribe(logService.TraceError).DisposeWith(disposables);
+    }
+
+    protected override void CreateCommands()
+    {
+        base.CreateCommands();
+
+        SelectHighCommand = ReactiveCommand.CreateFromTask(SelectHighAsync);
+        SelectMediumCommand = ReactiveCommand.CreateFromTask(SelectMediumAsync);
+        SelectLowCommand = ReactiveCommand.CreateFromTask(SelectLowAsync);
+    }
+
+    private Task SelectHighAsync()
+    {
+        return navigationService.NavigateToGame(SelectedTheme, Level.High);
+    }
+
+    private Task SelectMediumAsync()
+    {
+        return navigationService.NavigateToGame(SelectedTheme, Level.Medium);
+    }
+
+    private Task SelectLowAsync()
+    {
+        return navigationService.NavigateToGame(SelectedTheme, Level.Low);
+    }
 }
 
